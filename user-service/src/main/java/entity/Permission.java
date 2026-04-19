be92741783ce4e0bf1e54aff92b52f90f98845f2
ChamreunVira <virachamreun@gmail.com>
2026-04-19 15:35:00 +0700

    _

--- user-service/src/main/java/entity/Permission.java
+++ user-service/src/main/java/entity/Permission.java
@@ -17,6 +17,8 @@ public class Permission {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String test;
+
     @Column(name = "name")
     private String name;
 
